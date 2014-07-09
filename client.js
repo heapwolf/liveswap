f77bfe320da1a266edbabfbb50267c9150083c79
Paolo Fragomeni <paolo@async.ly>
2014-07-09 17:18:16 -0400

    makes api a little more flexible

--- client.js
+++ client.js
@@ -7,25 +7,26 @@ var net = require('net')
 // send a command to the liveswap server over tcp
 // using a simple JSON protocol.
 //
-var exec = exports.exec = function(command, value, port, cb) {
+var exec = exports.exec = function(args) {
 
-
-  if (typeof command == 'object') {
-    value = command[0]
-    port = command[1]
-    cb = command[2]
-    cmd = command.callee.name
+  if (typeof args[0] == 'function') {
+    options = {}
+  }
+  else if (typeof args[0] == 'string') {
+    options = { value: args[0] }
+  }
+  else {
+    options = args[0] || {}
   }
 
+  var cmd = options.cmd || args.callee.name
+  var value = options.value || ''
+  var port = options.port || 3000
+  var host = options.host || '127.0.0.1'
+  var cb = args[1]
 
-  if (typeof port == 'function') {
-    cb = port
-    port = null
-  }
-
-
-  var stream = net.connect(port || 3000, function() {
-    var message = { cmd: cmd, value: value || '' }
+  var stream = net.connect(port, host, function() {
+    var message = { cmd: cmd, value: value }
     stream.write(JSON.stringify(message) + '\n')
   })
 
